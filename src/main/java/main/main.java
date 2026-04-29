e2459e32b966a17117ef6577bcf5ee7625ffcc5f
Miguelchishman <miguelmatiasc09382@gmail.com>
2026-04-29 11:11:08 -0300

    Modelo

---
+++ src/main/java/main/main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+
+import view.Login;
+/**
+ *
+ * @author Miguel
+ */
+public class main {
+    public static void main(String[] args){
+        Login l = new Login();
+        l.setVisible(true);
+    }
+}
